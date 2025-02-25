5f86df408493af31d06e562683dbaaa91b713a56
Alexandr <rebplansh@gmail.com>
2025-02-25 14:49:25 +0800

    3

---
+++ Лабораторные/Лаб5/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+public class Student
+{
+    public string Name { get; private set; }
+    public List<Grade> Grades { get; private set; }
+
+    public Student(string name)
+    {
+        Name = name;
+        Grades = new List<Grade>();
+    }
+
+    public void AddGrade(Grade grade)
+    {
+        Grades.Add(grade);
+    }
+}
---
+++ Лабораторные/Лаб5/StudentReport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+
+public class StudentReport
+{
+    public string Name { get; set; }
+    public List<Grade> Grades { get; set; }
+
+    public StudentReport(Student student)
+    {
+        Name = student.Name;
+        Grades = student.Grades;
+    }
+
+    public static void SerializeReportsToJson(List<Student> students, string filePath)
+    {
+        var reports = new List<StudentReport>();
+
+        foreach (var student in students)
+        {
+            reports.Add(new StudentReport(student));
+        }
+
+        string json = JsonConvert.SerializeObject(reports, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText(filePath, json);
+        Console.WriteLine($"Отчет успешно сохранен в {filePath}");
+    }
+}
